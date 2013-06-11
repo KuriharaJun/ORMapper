5c7d766326dbe0e735f687baced0eee14f7c8ea9
Kurihara Jun <lh182051+src@gmail.com>
2013-06-11 22:17:14 +0900

    SQLコマンド識別用Enum型の追加

---
+++ MtrSclTs8DataMapper/MtrSclTs8DataMapper/EnSqlCommandType.cs
@@ -0,0 +1,13 @@
+﻿namespace KORMapper
+{
+    /// <summary>
+    /// 実行するSQLコマンドの種類
+    /// </summary>
+    public enum EnSqlCommandType
+    {
+        SELECT,
+        UPDATE,
+        INSERT,
+        DELETE,
+    }
+}
