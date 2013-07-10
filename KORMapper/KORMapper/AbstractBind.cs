44f9d7eeea927344dfe2f5e9b98b43a9c0bb8892
Kurihara Jun <lh182051+src@gmail.com>
2013-07-11 01:26:50 +0900

    DIクラスの追加

---
+++ KORMapper/KORMapper/AbstractBind.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KORMapper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class AbstractBind
+    {
+        /// <summary>
+        /// バインド情報格納ディクショナリ
+        /// </summary>
+        protected static Dictionary<string, Type> bindDictionary = new Dictionary<string, Type>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected internal abstract AbstractBind Bind();
+
+        /// <summary>
+        /// モデルの名称との紐付け
+        /// </summary>
+        /// <param name="t">依存関係情報</param>
+        /// <returns>現在のインスタンス</returns>
+        protected AbstractBind Named(Type t)
+        {
+            // tよりInject属性のNameを取得する
+            //bindDictionary.Add(name, t);
+
+            return this;
+        }
+    }
+}
---
+++ KORMapper/KORMapper/InjectAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KORMapper
+{
+    /// <summary>
+    /// 依存属性クラス
+    /// <para>クラス・インターフェース・構造体に対して設定する</para>
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// [Inject(Name = "injectName")]
+    /// </code>
+    /// </example>
+    public class InjectAttribute : Attribute
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// シングルトン
+        /// </summary>
+        public bool Singleton { get; set; }
+
+        /// <summary>
+        /// 実装
+        /// </summary>
+        public Type ToImple { get; set; }
+    }
+}
