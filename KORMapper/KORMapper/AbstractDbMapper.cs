1cfe7db938e5718e70ea7c52bfd8bcbb73e735a0
Kurihara Jun <lh182051+src@gmail.com>
2013-07-15 16:32:33 +0900

    マッピングの実�

--- KORMapper/KORMapper/AbstractDbMapper.cs
+++ KORMapper/KORMapper/AbstractDbMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -20,7 +21,7 @@ namespace KORMapper
         /// <param name="value">設定値</param>
         /// <param name="typeObject">設定対象オブジェクト</param>
         /// <param name="toType">変換型</param>
-        private void SetProperty<T>(PropertyInfo pi, object value, T typeObject, Type toType)
+        protected void SetProperty<T>(PropertyInfo pi, object value, T typeObject, Type toType)
         {
             if (toType == typeof(bool))
             {
@@ -261,6 +262,34 @@ namespace KORMapper
         }
 
         /// <summary>
+        /// プロパティに値を設定
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="columns"></param>
+        /// <param name="value"></param>
+        /// <param name="typeObject"></param>
+        protected bool SetPropertyValue<T>(string columns, object value, T typeObject)
+        {
+            PropertyInfo[] arPi = typeof(T).GetProperties();
+
+            foreach (var pi in arPi)
+            {
+                var at = (DataMapperAttribute)Attribute.GetCustomAttribute(pi, typeof(DataMapperAttribute));
+
+                if (at != null)
+                {
+                    if (at.Column == columns)
+                    {
+                        SetProperty<T>(pi, value, typeObject, pi.PropertyType);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
         /// フィールドへの値設定（型変換実行）
         /// </summary>
         /// <typeparam name="T">対象型</typeparam>
@@ -268,7 +297,7 @@ namespace KORMapper
         /// <param name="value">設定値</param>
         /// <param name="typeObject">設定対象オブジェクト</param>
         /// <param name="toType">変換型</param>
-        private void SetField<T>(FieldInfo fi, object value, T typeObject, Type toType)
+        protected void SetField<T>(FieldInfo fi, object value, T typeObject, Type toType)
         {
             if (toType == typeof(bool))
             {
@@ -433,6 +462,190 @@ namespace KORMapper
                     fi.SetValue(typeObject, null);
                 }
             }
+        }
+
+        /// <summary>
+        /// フィールドに値を設定
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="columns"></param>
+        /// <param name="value"></param>
+        /// <param name="typeObject"></param>
+        protected bool SetFieldValue<T>(string columns, object value, T typeObject)
+        {
+            FieldInfo[] arFi = typeof(T).GetFields();
+
+            foreach (var fi in arFi)
+            {
+                var at = (DataMapperAttribute)Attribute.GetCustomAttribute(fi, typeof(DataMapperAttribute));
+
+                if (at != null)
+                {
+                    if (at.Column == columns)
+                    {
+                        SetField<T>(fi, value, typeObject, fi.MemberType.GetType());
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        ///// <summary>
+        ///// 対象型のプロパティ・フィールド属性値をすべて取得
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <returns></returns>
+        //protected Dictionary<string, List<string>> GetAttribute<T>() where T : new()
+        //{
+        //    T t = new T();
+        //    Dictionary<string, List<string>> tableMapper = new Dictionary<string, List<string>>();
+
+        //    GetPropertyAttribute<T>(tableMapper);
+        //    GetFieldAttribute<T>(tableMapper);
+
+        //    return tableMapper;
+        //}
+
+        ///// <summary>
+        ///// プロパティの属性値をすべて取得
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <param name="tableMapper"></param>
+        ///// <param name="typeObject"></param>
+        //protected void GetPropertyAttribute<T>(Dictionary<string, List<string>> tableMapper)
+        //{
+        //    PropertyInfo[] arPi = typeof(T).GetProperties();
+        //    foreach (PropertyInfo pi in arPi)
+        //    {
+        //        DataMapperAttribute at = (DataMapperAttribute)Attribute.GetCustomAttribute(pi, typeof(DataMapperAttribute));
+
+        //        if (at == null)
+        //        {
+        //            // 属性設定なしの場合は無視
+        //        }
+        //        else if (tableMapper.ContainsKey(at.Table) == true)
+        //        {
+        //            tableMapper[at.Table].Add(at.Column);
+        //        }
+        //        else
+        //        {
+        //            var entities = new List<string>();
+        //            entities.Add(at.Column);
+        //            tableMapper.Add(at.Table, entities);
+        //        }
+        //    }
+        //}
+
+        ///// <summary>
+        ///// フィールドの属性値をすべて取得
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <param name="tableMapper"></param>
+        ///// <param name="typeObject"></param>
+        //protected void GetFieldAttribute<T>(Dictionary<string, List<string>> tableMapper)
+        //{
+        //    FieldInfo[] arFi = typeof(T).GetFields(BindingFlags.GetField);
+
+        //    foreach (FieldInfo fi in arFi)
+        //    {
+        //        DataMapperAttribute at = (DataMapperAttribute)Attribute.GetCustomAttribute(fi, typeof(DataMapperAttribute));
+        //        if (tableMapper.ContainsKey(at.Table) == true)
+        //        {
+        //            tableMapper[at.Table].Add(at.Column);
+        //        }
+        //        else
+        //        {
+        //            var entities = new List<string>();
+        //            entities.Add(at.Column);
+        //            tableMapper.Add(at.Table, entities);
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// データアクセス層からデータを読み出し、格納型Listに設定する
+        /// </summary>
+        /// <typeparam name="T">格納型</typeparam>
+        /// <param name="da">データアクセス層</param>
+        /// <returns></returns>
+        protected List<T> GetData<T>(DataTable da)
+        {
+            List<T> dataList = null;
+
+            // DataTable 設定なし
+            if (da == null)
+            {
+                return dataList;
+            }
+            else if (Injector.MappingData.ContainsKey(da.TableName) == false)
+            {
+                return dataList;
+            }
+            else
+            {
+                // マッピングキーの取得
+                var keyDic = Injector.MappingData[da.TableName];
+
+                dataList = new List<T>();
+                foreach (DataRow dr in da.Rows)
+                {
+                    var keyList = this.GetColumnList(keyDic);
+
+                    // 値の設定
+                    this.SetValue(dataList, dr, keyList);
+                }
+                return dataList;
+            }
+        }
+
+        /// <summary>
+        /// マッピングテーブルからカラムキーリスト取得
+        /// </summary>
+        /// <param name="keyDic">マッピングテーブル</param>
+        /// <returns>カラムキーリスト</returns>
+        private List<string> GetColumnList(Dictionary<string,object> keyDic)
+        {
+            // マッピングテーブルにキー[Column]が存在しない
+            if (keyDic.ContainsKey("Column") == false)
+            {
+                throw new KORMapper.Exception.KORMapperException("MappingTable don't have Column key.");
+            }
+
+            // 型チェック
+            var keyList = keyDic["Column"] as List<string>;
+
+            if (keyList == null)
+            {
+                throw new InvalidCastException("MappingTable doesn't cast object to List<string>type.");
+            }
+
+            return keyList;
+        }
+
+        /// <summary>
+        /// DB値設定
+        /// </summary>
+        /// <typeparam name="T">要素型</typeparam>
+        /// <param name="dataList">設定先リスト</param>
+        /// <param name="dr">データ</param>
+        /// <param name="keyList">キーリスト</param>
+        private void SetValue<T>(List<T> dataList, DataRow dr, List<string> keyList)
+        {
+            T model = Activator.CreateInstance<T>();
+
+            foreach (string key in keyList)
+            {
+                object o = dr[key];
+                if (SetPropertyValue<T>(key, o, model) == false)
+                {
+                    if (SetFieldValue<T>(key, o, model) == false)
+                    {
+                        // 無視
+                    }
+                }
+            }
+            dataList.Add(model);
         }
     }
 }
--- KORMapper/KORMapper/Injector.cs
+++ KORMapper/KORMapper/Injector.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace KORMapper
 {
@@ -10,19 +8,186 @@ namespace KORMapper
     /// </summary>
     public class Injector
     {
-        public Injector(Type bind)
+        /// <summary>
+        /// モデルマッピングDictionary
+        /// </summary>
+        private static Dictionary<string, Dictionary<string, object>> mappingData = new Dictionary<string,Dictionary<string,object>>();
+
+        /// <summary>
+        /// モデル名・テーブル名マッピング
+        /// </summary>
+        private static LinkedList<ObjectLinkNode> nameMapList = new LinkedList<ObjectLinkNode>();
+
+        /// <summary>
+        /// モデルマッピングDictionary
+        /// </summary>
+        public static Dictionary<string, Dictionary<string, object>> MappingData { get; private set; }
+
+        /// <summary>
+        /// マッピング情報設定
+        /// </summary>
+        /// <exception cref="InvalidCastException">DataMapper属性にキャスト不可能</exception>
+        /// <exception cref="NullReferenceException">DataMapper属性が設定されていない。もしくは、Tableパラメータが設定されていない。</exception>
+        public static void SetMapping()
         {
-            // bindより依存Modelの情報取得
+            var dDic = AbstractBind.BindDictionary;
+
+            foreach (string s in dDic.Keys)
+            {
+                object[] arAt = dDic[s].GetCustomAttributes(typeof(DataMapperAttribute), true);
+                if (arAt != null)
+                {
+                    foreach (var at in arAt)
+                    {
+                        var dAt = at as DataMapperAttribute;
+
+                        if (dAt == null)
+                        {
+                            throw new InvalidCastException("Attribute can't cast to DataMapperAttribute.");
+                        }
+
+                        if (string.IsNullOrEmpty(dAt.Table) == true)
+                        {
+                            throw new NullReferenceException("this Table is not exists.");
+                        }
+
+                        if (mappingData.ContainsKey(dAt.Table) == true)
+                        {
+                            var mapData = mappingData[dAt.Table];
+                            SetMappingInfo(mapData, dAt);
+                            mappingData[dAt.Table] = mapData;
+                        }
+                        else
+                        {
+                            var mapData = new Dictionary<string, object>();
+                            SetMappingInfo(mapData, dAt);
+                            mappingData.Add(dAt.Table, mapData);
+
+                            var node = new ObjectLinkNode(s, dAt.Table);
+                            nameMapList.AddLast(node);
+                        }
+                    }
+                }
+                else
+                {
+                    throw new NullReferenceException("DataMapperAttribute is not exists.\nPlease set DatamapperAttribute.");
+                }
+            }
         }
 
-        public IDBMapper GetMapping()
+        /// <summary>
+        /// モデルマッピング情報設定
+        /// </summary>
+        /// <param name="data">マッピング情報格納ディクショナリ</param>
+        /// <param name="attribute">属性</param>
+        private static void SetMappingInfo(Dictionary<string, object> data, DataMapperAttribute attribute)
         {
-            return null;
+            SetColumnValue(data, attribute.Column);
+            SetParamValue(data, attribute.Param);
+            SetParamOrder(data, attribute.ParamOrder);
         }
 
-        public T GetMapping<T>() where T : new()
+        /// <summary>
+        /// カラム情報設定
+        /// </summary>
+        /// <param name="data">マッピング情報格納ディクショナリ</param>
+        /// <param name="column">カラム</param>
+        /// <exception cref="InvalidCastException">キー[Column]に対して無効なデータ型が登録されている</exception>
+        private static void SetColumnValue(Dictionary<string, object> data, string column)
         {
-            return default(T);
+            if (string.IsNullOrEmpty(column) == false)
+            {
+                if (data.ContainsKey("Column") == true)
+                {
+                    var val = data["Column"] as List<string>;
+                    if (val == null)
+                    {
+                        throw new InvalidCastException("Data can't cast to List<string>.");
+                    }
+
+                    val.Add(column);
+                    data["Column"] = val;
+                }
+                else
+                {
+                    var val = new List<string>();
+                    val.Add(column);
+                    data.Add("Column", val);
+                }
+            }
+        }
+
+        /// <summary>
+        /// パラメータ情報設定
+        /// </summary>
+        /// <param name="data">マッピング情報格納ディクショナリ</param>
+        /// <param name="param">パラメータ</param>
+        /// <exception cref="InvalidCastException">キー[Param]に対して無効なデータ型が登録されている</exception>
+        private static void SetParamValue(Dictionary<string, object> data, string param)
+        {
+            if (string.IsNullOrEmpty(param) == false)
+            {
+                if (data.ContainsKey("Param") == true)
+                {
+                    var val = data["Param"] as List<string>;
+                    if (val == null)
+                    {
+                        throw new InvalidCastException("Data can't cast to List<string>.");
+                    }
+
+                    val.Add(param);
+                    data["Param"] = val;
+                }
+                else
+                {
+                    var val = new List<string>();
+                    val.Add(param);
+                    data.Add("Param", val);
+                }
+            }
+        }
+
+        /// <summary>
+        /// パラメータ順序設定
+        /// </summary>
+        /// <param name="data">マッピング情報格納ディクショナリ</param>
+        /// <param name="paramOrder">パラメータ順序</param>
+        /// <exception cref="InvalidCastException">キー[ParamOrder]に対して無効なデータ型が登録されている</exception>
+        private static void SetParamOrder(Dictionary<string, object> data, uint? paramOrder)
+        {
+            if (paramOrder.HasValue == true)
+            {
+                if (data.ContainsKey("ParamOrder") == true)
+                {
+                    var val = data["ParamOrder"] as List<uint>;
+                    if (val == null)
+                    {
+                        throw new InvalidCastException("Data can't cast to List<uint>.");
+                    }
+
+                    val.Add(paramOrder.Value);
+                    data["ParamOrder"] = val;
+                }
+                else
+                {
+                    var val = new List<uint>();
+                    val.Add(paramOrder.Value);
+                    data.Add("ParamOrder", val);
+                }
+            }
+        }
+
+        private struct ObjectLinkNode
+        {
+            public string NodeName { get; private set; }
+            public string TableName { get; private set; }
+
+            public ObjectLinkNode(string nodeName, string tableName)
+                :this()
+            {
+                this.NodeName = nodeName;
+                this.TableName = tableName;
+            }
         }
     }
 }
