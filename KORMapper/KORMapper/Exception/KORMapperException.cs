fd788d059f1a1257ecfe36323bd73b30dffdf4c1
Kurihara Jun <lh182051+src@gmail.com>
2013-07-15 15:33:54 +0900

    KORMapperExceptionの追加

---
+++ KORMapper/KORMapper/Exception/KORMapperException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KORMapper.Exception
+{
+    public class KORMapperException : System.Exception
+    {
+        public KORMapperException() { }
+
+        public KORMapperException(string message) : base(message) { }
+
+        public KORMapperException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
