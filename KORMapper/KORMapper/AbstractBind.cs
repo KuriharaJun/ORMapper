9160ab49864613a6f2019c613bfa50b05d92b2a6
Kurihara Jun <lh182051+src@gmail.com>
2013-07-15 15:37:31 +0900

    AbstractBindの実�

--- KORMapper/KORMapper/AbstractBind.cs
+++ KORMapper/KORMapper/AbstractBind.cs
@@ -8,7 +8,7 @@ namespace KORMapper
     /// <summary>
     /// 
     /// </summary>
-    public abstract class AbstractBind
+    public class AbstractBind
     {
         /// <summary>
         /// バインド情報格納ディクショナリ
@@ -16,10 +16,18 @@ namespace KORMapper
         protected static Dictionary<string, Type> bindDictionary = new Dictionary<string, Type>();
 
         /// <summary>
+        /// バインド情報格納ディクショナリプロパティ
+        /// </summary>
+        public static Dictionary<string, Type> BindDictionary { get; private set; }
+
+        /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        protected internal abstract AbstractBind Bind();
+        protected internal virtual AbstractBind Bind()
+        {
+            return this;
+        }
 
         /// <summary>
         /// モデルの名称との紐付け
@@ -28,8 +36,31 @@ namespace KORMapper
         /// <returns>現在のインスタンス</returns>
         protected AbstractBind Named(Type t)
         {
-            // tよりInject属性のNameを取得する
-            //bindDictionary.Add(name, t);
+            object[] arAt = t.GetCustomAttributes(typeof(InjectAttribute), true);
+
+            if (arAt == null)
+            {
+                throw new NullReferenceException("InjectAttribute is not exists.");
+                
+            }
+            else
+            {
+                foreach (var o in arAt)
+                {
+                    var io = o as InjectAttribute;
+                    if (io == null)
+                    {
+                        throw new InvalidCastException("Attribute can't cast to InjectAttribute.");
+                    }
+
+                    if (bindDictionary.ContainsKey(io.Name) == true)
+                    {
+                        throw new ArgumentException("this Name is exists. Name is " + io.Name);
+                    }
+
+                    bindDictionary.Add(io.Name, t);
+                }
+            }
 
             return this;
         }
